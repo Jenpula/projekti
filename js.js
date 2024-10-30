82b7ab0bbe791f2e179923a94c4adcb1e5ef389f
Jenpula <jenniannala11@hotmail.com>
2024-10-30 12:03:20 +0200

    Poistettu turhaa koodia

--- js.js
+++ js.js
@@ -2,9 +2,7 @@ window.onscroll = function() {
     const backToTopBtn = document.getElementById("backToTopBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopBtn.style.display = "block";
-        backToTopBtn.classList.add('show');
 } else {
-    backToTopBtn.classList.remove('show');
     backToTopBtn.style.display = "none";
 }
 }
