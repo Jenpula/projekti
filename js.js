5dcbbafcaa2b33bd5f7c25344713e888585d09fe
Jenpula <jenniannala11@hotmail.com>
2024-10-30 12:00:11 +0200

    Funktionaalisuutta napille, jotta sitä painamalla sivu vierittyisi hitaasti.

--- js.js
+++ js.js
@@ -2,13 +2,16 @@ window.onscroll = function() {
     const backToTopBtn = document.getElementById("backToTopBtn");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopBtn.style.display = "block";
+        backToTopBtn.classList.add('show');
 } else {
-
+    backToTopBtn.classList.remove('show');
     backToTopBtn.style.display = "none";
 }
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({
+        top: 0,
+        behaviour: "smooth"
+    });
 }
