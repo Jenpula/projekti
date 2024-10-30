7f735313ad387757e6a63f3c21383c6919ab59d6
Jenpula <jenniannala11@hotmail.com>
2024-10-30 11:48:28 +0200

    linkitetty js tiedosto html osioon ja siirretty index.html kansiosta JavaScriptit omaan tiedostoon

--- js.js
+++ js.js
@@ -1,2 +1,16 @@
 
 
+window.onscroll = function() {
+    const backToTopBtn = document.getElementById("backToTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopBtn.style.display = "block";
+} else {
+
+    backToTopBtn.style.display = "none";
+}
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+}
